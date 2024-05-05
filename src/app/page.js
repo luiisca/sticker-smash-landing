7fd29b9f40d80b71be9f81cad27d7008c4c6b1fc
Luis Cadillo <luiscaf3r@gmail.com>
2024-05-04 23:14:31 -0500

    landing done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,82 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const features = [
+    {
+        title: "Pick Images with Ease",
+        content: "Select photos from your gallery effortlessly to start customizing."
+    },
+    {
+        title: "Add Stickers in a Tap",
+        content: "Browse and add fun stickers to your images with a simple tap."
+    },
+    {
+        title: "Save to Device Gallery",
+        content: "Save your personalized images directly to your device for easy access."
+    }
+]
+
+function Card({ title, content }) {
+    return (
+        <div className="py-4 px-5 rounded-xl transition-all text-center">
+            <h2 className="text-lg md:text-xl font-inter font-semibold mb-3">
+                {title}
+            </h2>
+            <p className="font-inter m-0 opacity-60 leading-normal text-subtle max-w-xs">
+                {content}
+            </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    )
 }
+const IndexPage = () => {
+    return (
+        <main className="flex space-y-12 flex-col justify-between items-center p-16 md:py-24 max-w-[1500px] mx-auto min-h-screen overflow-x-hidden">
+            <div className="flex w-full items-center justify-between max-sm:flex-col max-sm:space-y-2 ">
+                <div className="relative">
+                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold font-cal">
+                        StickerSmash
+                    </h1>
+                    <div className="absolute right-0 translate-x-[80%] md:translate-x-[65%] top-0 -translate-y-[45%] rotate-[24deg] pt-1 pb-2.5">
+                        <div className="relative flex items-center justify-center">
+                            <div class="text-[2.5rem] md:text-[4.5rem] bg-clip-text text-transparent bg-white py-2 md:py-4">🔥</div>
+                            <div className="absolute text-[2rem] md:text-[4rem]">🔥</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <a className="hidden lg:block text-foreground hover:text-foreground-emphasis hover:underline hover:underline-offset-4 w-fit text-black h-fit items-center group justify-between space-x-2 md:text-xl font-inter font-bold py-2 px-4 md:py-3 md:px-6 rounded-lg transition-all" href="https://github.com/luiisca/sticker-smash/releases/download/v1.0/sticker-smash.apk">
+                        Download
+                    </a>
+                    <a className="max-md:ml-4 text-black h-fit flex items-center group justify-between space-x-2 md:text-xl font-inter font-bold py-2 px-4 md:py-3 md:px-6 rounded-lg bg-brand hover:bg-brand-emphasis transition-all" href="https://luiisca.github.io/sticker-smash/" target="_blank">
+                        <span className="transition-all">Go to App</span>
+                        <svg className="transition-all duration-200 group-hover:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
+                    </a>
+                </div>
+            </div>
+            <div className="flex flex-col space-y-6 w-full flex-grow min-h-[50vh] items-center">
+                <div className="flex-grow w-full flex justify-center items-center relative">
+                    <div className="left-1/2 absolute blur-[45px] translate-z-0 bg-secondary-glow rounded-[50%] w-[580px] h-[460px] -ml-[500px] -z-10" />
+                    <Image
+                        src="/app.png"
+                        alt="Whop W"
+                        fill
+                        objectFit="contain"
+                        priority
+                    />
+                    <div className="left-[40%] absolute blur-[45px] translate-z-0 bg-primary-glow w-[300px] h-[240px] -z-20" />
+                </div>
+                <a className="lg:hidden w-fit text-black h-fit flex items-center group justify-between space-x-2 md:text-xl font-inter font-bold py-2 px-4 md:py-3 md:px-6 rounded-lg bg-brand hover:bg-brand-emphasis transition-all" href="https://github.com/luiisca/sticker-smash/releases/download/v1.0/sticker-smash.apk">
+                    <span>Download</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="7 10 12 15 17 10" /><line x1="12" x2="12" y1="15" y2="3" /></svg>
+                </a>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {features.map((feat, index) =>
+                    <Card key={index} title={feat.title} content={feat.content} />
+                )}
+            </div>
+        </main >
+    );
+};
+
+export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,56 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "primary-glow": "var(--primary-glow)",
+                "secondary-glow": "var(--secondary-glow)",
+            },
+            fontFamily: {
+                inter: ["var(--font-inter)"],
+                cal: ["var(--font-cal)"],
+            },
+            colors: {
+                muted: "hsl(var(--muted))",
+                subtle: "hsl(var(--subtle))",
+                emphasis: "hsl(var(--emphasis))",
+                brand: {
+                    emphasis: "hsl(var(--brand-emphasis))",
+                    secondary: "hsl(var(--brand-secondary))",
+                    secondaryEmphasis: "hsl(var(--brand-secondary-emphasis))",
+                    DEFAULT: "hsl(var(--brand))",
+                },
+                foreground: {
+                    emphasis: "hsl(var(--text-emphasis))",
+                    DEFAULT: "hsl(var(--text))"
+                },
+            },
+            textColor: {
+                subtle: "hsl(var(--text-subtle))",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        plugin(function({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    'translate-z': (value) => ({
+                        '--tw-translate-z': value,
+                        transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+                    }), // this is actual CSS
+                },
+                { values: theme('translate'), supportsNegativeValues: true }
+            )
+        })
+    ],
 };
